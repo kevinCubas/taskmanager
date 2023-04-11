b1e6035ef6dbfb77cd3aefeccdbfba74e361e64b
unknown <61201131+kevinCubas@users.noreply.github.com>
2023-04-11 16:38:29 -0300

    style: create global style

---
+++ src/style/global.ts
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --gray-100: #F2F2F2;
+    --gray-200: #D9D9D9;
+    --gray-300: #808080;
+    --gray-400: #333333;
+    --gray-500: #262626;
+    --gray-600: #1A1A1A;
+    --gray-700: #0D0D0D;
+
+    --blue: #4EA8DE;
+    --blue-dark: #1E6F9F;
+
+    --purple: #8284FA;
+    --purple-dark: #5E60CE;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  // font-size normal === 16px
+  // Accessibility
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%; // 15px
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%; // 14px
+    }
+  }
+
+  body {
+    font-family: 'Inter', sans-serif;
+    background-color: var(--gray-600);
+  }
+`
